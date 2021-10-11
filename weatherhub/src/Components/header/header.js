0b4db1360359f2ff20f68d21c5adc488a2e7a28c
zbattaglia <zbattaglia3@gmail.com>
2021-10-10 20:47:50 -0500

    Set up initial header

---
+++ weatherhub/src/Components/header/header.js
@@ -0,0 +1,13 @@
+import './header.css'
+
+function Header() {
+    return (
+        <header id="App-header">
+          <p>
+            This Weather Hub
+          </p>
+        </header>
+    );
+  }
+  
+  export default Header;
