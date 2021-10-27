5b5035c63dc96fb3afc3a3d835c8824db695ff39
zbattaglia <zbattaglia3@gmail.com>
2021-10-26 20:20:28 -0500

    Built out external API call

--- weatherhub/server/routes/weatherRouter.js
+++ weatherhub/server/routes/weatherRouter.js
@@ -1,27 +1,31 @@
-const express = require('express')
+const express = require('express');
 const router = express.Router();
-
-// Dummy Weather Response
-let weather = [{cityName: 'Minneapolis', weather: 'Sunny', temp: '75'},
-                {cityName: 'Chicago', weather: 'Parlty Sunny', temp: '70'},
-                {cityName: 'Green Bay', weather: 'Rainy', temp: '60'}
-            ]
+// axios for making API CALL
+const axios = require('axios');
+// dotenv is for accessing environmental variables. config reads the .env file in.
+const dotenv = require('dotenv');
+dotenv.config();
 
 // for a get request on the 'weather' route, return our forecast info
 router.get('/:location', (req, res) => {
+    // get the location passed in with the request parameters
     const location = req.params.location;
-    let response = {};
-    console.log("Weather", weather[0].cityName)
-    for (let i = 0; i < weather.length; i++){
-        console.log('In Weather array', weather[i].cityName);
-        if(weather[i].cityName == location){
-            response = weather[i];
+    
+    console.log( 'Getting weather on the server for', location );
+    // async function to make a call to the Open Weather API for the selected location using the APIKEY stored in .env file
+    const getWeather = async( location ) => {
+        try{
+            const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${process.env.APIKEY}`);
+            console.log('Got a response', response.data);
+            
+        } catch(error) {
+            console.log('Error getting the weather from API', error);
         }
     }
     
-    console.log('Got request for weather on the server at', req.params.location);
-    console.log('Sending data back', response)
-    res.send(response);
+    // Call getWeather and pass in location
+    getWeather( location );
+
 })
 
 module.exports = router;
--- weatherhub/src/Components/Forecast/forecast.js
+++ weatherhub/src/Components/Forecast/forecast.js
@@ -3,17 +3,14 @@ import './forecast.css'
 // import Row from 'react-bootstrap/Row'
 // import Col from 'react-bootstrap/Col'
 
-import ButtonCount from '../ButtonCount/ButtonCount';
-import NameSaver from '../NameSaver/NameSaver'
-import GetWeather from '../GetWeather/GetWeather';
+// import ButtonCount from '../ButtonCount/ButtonCount';
+// import NameSaver from '../NameSaver/NameSaver'
+// import GetWeather from '../GetWeather/GetWeather';
 
 function Forecast() {
     return (
         <div>
             <p>The weather forecast goes here</p>
-            <ButtonCount />
-            <NameSaver />
-            <GetWeather />
         </div>
     );
   }
